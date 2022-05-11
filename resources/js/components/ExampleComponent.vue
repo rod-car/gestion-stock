b8637a268f82786924d89343305cb66ff581cd37
rod-car <ghislaincarino@gmail.com>
2022-05-11 10:24:50 +0300

    First commit

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,145 +1,16 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae ut molestiae quae alias illum a commodi, animi laboriosam magni doloremque, temporibus, sit accusamus consequatur id? Alias ducimus qui unde.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id neque repudiandae tempore magnam deleniti quasi. Ullam iusto repudiandae explicabo voluptatem quod in accusamus sint nostrum, aut vitae quis amet dolorem!
-
-                    </div>
+            <div class="card p-5">
+                <div v-show="loading">
+                    Loading
                 </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae ut molestiae quae alias illum a commodi, animi laboriosam magni doloremque, temporibus, sit accusamus consequatur id? Alias ducimus qui unde.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id neque repudiandae tempore magnam deleniti quasi. Ullam iusto repudiandae explicabo voluptatem quod in accusamus sint nostrum, aut vitae quis amet dolorem!
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae ut molestiae quae alias illum a commodi, animi laboriosam magni doloremque, temporibus, sit accusamus consequatur id? Alias ducimus qui unde.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id neque repudiandae tempore magnam deleniti quasi. Ullam iusto repudiandae explicabo voluptatem quod in accusamus sint nostrum, aut vitae quis amet dolorem!
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae ut molestiae quae alias illum a commodi, animi laboriosam magni doloremque, temporibus, sit accusamus consequatur id? Alias ducimus qui unde.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id neque repudiandae tempore magnam deleniti quasi. Ullam iusto repudiandae explicabo voluptatem quod in accusamus sint nostrum, aut vitae quis amet dolorem!
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae ut molestiae quae alias illum a commodi, animi laboriosam magni doloremque, temporibus, sit accusamus consequatur id? Alias ducimus qui unde.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id neque repudiandae tempore magnam deleniti quasi. Ullam iusto repudiandae explicabo voluptatem quod in accusamus sint nostrum, aut vitae quis amet dolorem!
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae ut molestiae quae alias illum a commodi, animi laboriosam magni doloremque, temporibus, sit accusamus consequatur id? Alias ducimus qui unde.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id neque repudiandae tempore magnam deleniti quasi. Ullam iusto repudiandae explicabo voluptatem quod in accusamus sint nostrum, aut vitae quis amet dolorem!
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae ut molestiae quae alias illum a commodi, animi laboriosam magni doloremque, temporibus, sit accusamus consequatur id? Alias ducimus qui unde.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id neque repudiandae tempore magnam deleniti quasi. Ullam iusto repudiandae explicabo voluptatem quod in accusamus sint nostrum, aut vitae quis amet dolorem!
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae ut molestiae quae alias illum a commodi, animi laboriosam magni doloremque, temporibus, sit accusamus consequatur id? Alias ducimus qui unde.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id neque repudiandae tempore magnam deleniti quasi. Ullam iusto repudiandae explicabo voluptatem quod in accusamus sint nostrum, aut vitae quis amet dolorem!
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae ut molestiae quae alias illum a commodi, animi laboriosam magni doloremque, temporibus, sit accusamus consequatur id? Alias ducimus qui unde.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id neque repudiandae tempore magnam deleniti quasi. Ullam iusto repudiandae explicabo voluptatem quod in accusamus sint nostrum, aut vitae quis amet dolorem!
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae ut molestiae quae alias illum a commodi, animi laboriosam magni doloremque, temporibus, sit accusamus consequatur id? Alias ducimus qui unde.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id neque repudiandae tempore magnam deleniti quasi. Ullam iusto repudiandae explicabo voluptatem quod in accusamus sint nostrum, aut vitae quis amet dolorem!
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae ut molestiae quae alias illum a commodi, animi laboriosam magni doloremque, temporibus, sit accusamus consequatur id? Alias ducimus qui unde.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id neque repudiandae tempore magnam deleniti quasi. Ullam iusto repudiandae explicabo voluptatem quod in accusamus sint nostrum, aut vitae quis amet dolorem!
-
-                    </div>
+                <div class="card-body" v-show="!loading">
+                    <ul>
+                        <div v-for="user in users" v-bind:key="user.id">
+                            <li class="text-uppercase">{{ user.id }}: {{ user.name }}</li>
+                        </div>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -147,9 +18,19 @@
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
-        mounted() {
-            
+        data() {
+            return {
+                users: null,
+                loading: true,
+            }
+        },
+        async mounted() {
+            let result = await axios('https://jsonplaceholder.typicode.com/users/')
+            this.loading = false
+            this.users = result.data
         }
     }
 </script>
