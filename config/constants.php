ef30480ca2372255dac8c5d02d973feb170b0415
rod-car <ghislaincarino@gmail.com>
2022-07-21 10:27:11 +0300

    Configuration de seeders et ajout des constantes des fonctions et roles

---
+++ config/constants.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'roles' => [
+        'add_point_vente' => 'Ajouter un nouveau point de vente',
+        'view_point_vente' => 'Voir la liste de point de vente',
+        'manage_roles_and_functions' => 'Gerer les personnels et les roles',
+        'add_user' => 'Ajouter un nouveau personnel',
+        'delete_user' => 'Supprimer un personnel',
+        'view_user' => 'Voir la liste des utilisateur',
+        'edit_user' => 'Modifier un personnel',
+        'edit_point_vente' => 'Modifier un point de vente',
+        'delete_point_vente' => 'Supprimer un point de vente',
+        'add_entrepot' => 'Ajouter une nouvelle entrepôt',
+        'view_entrepot' => 'Voir la liste des entrepôt',
+        'edit_entrepot' => 'Modifier un entrepôt',
+        'delete_entrepot' => 'Supprimer un entrepôt',
+        'manage_settings' => 'Gerer les parametres de l\'entreprise',
+    ],
+    'fonctions' => [
+        'Magasinier' => 'Responsable du magasin',
+        'Vendeur' => 'Responsable de vente',
+        'Directeur' => 'Responsable de tout',
+    ],
+];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Article\Article;
 use App\Models\Article\Categorie;
+use App\Models\Personnel\Fonction;
 use App\Models\Role\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -22,16 +23,45 @@ class DatabaseSeeder extends Seeder
         Article::factory(200)->create();
         Role::factory(20)->create();
 
-        $categories = Categorie::all();
-        $roles = Role::all();
+        $categories = Categorie::all(); */
 
-        User::all()->each(function ($user) use ($roles) {
+        $users = User::all();
+
+        foreach (config('constants.fonctions') as $name => $description) {
+            $fonction = Fonction::create([
+                'nom_fonction' => $name,
+                'description_fonction' => $description,
+            ]);
+        }
+
+        $fonctions = Fonction::all();
+
+        foreach (config('constants.roles') as $name => $description) {
+            $role = Role::create([
+                'nom_role' => $name,
+                'description' => $description,
+            ]);
+
+            foreach ($fonctions as $fonction) {
+                $fonction->permissions()->attach($role->id);
+            }
+
+            foreach ($users as $user) {
+                $user->roles()->attach($role->id);
+            }
+        }
+
+        foreach ($users as $user) {
+            $user->fonctions()->attach(rand(1, 3));
+        }
+
+        /*User::all()->each(function ($user) use ($roles) {
             $user->roles()->attach(
                 $roles->random(rand(1, 3))->pluck('id')->toArray()
             );
-        });
+        });*/
 
-        Article::all()->each(function ($article) use ($categories) {
+        /*Article::all()->each(function ($article) use ($categories) {
             $article->categories()->attach(
                 $categories->random(rand(1, 3))->pluck('id')->toArray()
             );
