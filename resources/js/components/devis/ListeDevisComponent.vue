883a9eabf6baf2e7254302d480f8e936c5bcd867
rod-car <ghislaincarino@gmail.com>
2022-08-26 16:54:20 +0300

    hot: fix casts for commande

--- resources/js/components/devis/ListeDevisComponent.vue
+++ resources/js/components/devis/ListeDevisComponent.vue
@@ -23,6 +23,7 @@
                 <td class="align-middle"><Status :value="parseInt(devis.status)" /></td>
 
                 <td class="d-flex justify-content-center">
+                    {{ parseInt(devis.status) }}
                     <router-link title="Voir ce devis" v-if="true" :to="{ name: `devis.${type}.voir`, params: { id: devis.id }}" class="btn btn-info btn-sm me-2 text-white"><i class="fa fa-eye"></i></router-link>
                     <router-link title="Modifier ce devis" v-if="true" :to="{ name: `devis.${type}.modifier`, params: { id: devis.id }}" class="btn btn-primary btn-sm me-2"><i class="fa fa-edit"></i></router-link>
                     <router-link title="Créer un bon de commande pour ce devis" v-if="parseInt(devis.recu) === 1" :to="{ name: `commande.${type}.nouveau`, query: { devis: devis.id }}" class="btn btn-warning btn-sm me-2 text-white"><i class="fa fa-arrow-right"></i></router-link>
